c9f165346cfea68d73dff0d08bcb388038188741
amalec <amalec@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2001-06-01 15:44:43 +0000

    GNUified source files with copyright notice
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@21 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- check/src/check_impl.h
+++ check/src/check_impl.h
@@ -1,6 +1,25 @@
 #ifndef CHECK_IMPL_H
 #define CHECK_IMPL_H
 
+/*
+  Check: a unit test framework for C
+  Copyright (C) 2001, Arien Malec
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 /* This header should be included by any module that needs
    to know the implementation details of the check structures
    Include list.h before this header
