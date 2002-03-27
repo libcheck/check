975a2e1795076a0a3d207babe5edd0b6222aa4f0
amalec <amalec@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2002-03-27 01:18:55 +0000

    Cleaned up CVS to remove all autofoo generated files, included an autogen.sh bootstrap file. Changes at the suggestion of Sven Neumann
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@122 64e312b2-a51f-0410-8e61-82d0ca0eb02a

---
+++ check/autogen.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+PROJECT="the Check unit test framework"
+TEST_TYPE=-f
+FILE=src/check.c
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+echo "I am testing that you have the required versions of autoconf and automake"
+echo
+
+echo "Testing autoconf... "
+VER=`autoconf --version | grep -iw autoconf | sed "s/.* \([0-9.]*\)[-a-z0-9]*$/\1/"`
+if expr $VER \>= 2.13 >/dev/null; then
+	echo "looks OK."
+else
+	echo "too old! (Need 2.13, have $VER)"
+	DIE=1
+fi
+
+echo "Testing automake... "
+VER=`automake --version | grep automake | sed "s/.* \([0-9.]*\)[-a-z0-9]*$/\1/"`
+if expr $VER \>= 1.4 >/dev/null; then
+	echo "looks OK."
+else
+	echo "too old! (Need 1.4, have $VER)"
+	DIE=1
+fi
+
+echo
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+test $TEST_TYPE $FILE || {
+	echo "You must run this script in the top-level $PROJECT directory"
+	exit 1
+}
+
+if test -z "$*"; then
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+case $CC in
+*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
+esac
+
+autogen_dirs="."
+
+for i in $autogen_dirs; do
+        echo "Processing $i..."
+
+        cd $i
+        # optionally feature autoheader
+        if grep AM_CONFIG_HEADER configure.in >/dev/null ; then
+                (autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
+        fi
+
+        automake --add-missing $am_opt
+        autoconf
+done
+
+cd $ORIGDIR
+
+$srcdir/configure --enable-maintainer-mode "$@"
+
+echo 
+echo "Now type 'make' to compile $PROJECT."
