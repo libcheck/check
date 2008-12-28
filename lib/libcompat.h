01485655d2969d97405f6ba0f9f85124e5e62eb5
cpickett <cpickett@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2008-12-28 23:00:21 +0000

    * silence warning when nothing is included in libcompat
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@473 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- lib/libcompat.h
+++ lib/libcompat.h
@@ -22,3 +22,6 @@ unsigned int sleep (unsigned int seconds);
 #if !HAVE_STRSIGNAL
 const char *strsignal(int sig);
 #endif /* !HAVE_STRSIGNAL */
+
+/* silence warnings about an empty library */
+void ck_do_nothing (void);
