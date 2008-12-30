c0394c26f80ec2f05728ab04cd7f21cfe90a3fcc
cpickett <cpickett@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2008-12-30 01:37:21 +0000

    * only check for those functions that we currently need to check for   to solve portability problems, and avoid checking for things where   we don't currently use the result generated in config.h * add stubs for setenv, unsetenv, localtime_r, and fileno
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@481 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- lib/libcompat.h
+++ lib/libcompat.h
@@ -7,25 +7,41 @@
 /* defines size_t */
 #include <sys/types.h>
 
+#if !HAVE_FILENO
+int fileno(FILE *stream);
+#endif /* !HAVE_FILENO */
+
+#if !HAVE_LOCALTIME_R
+struct tm *localtime_r(const time_t *clock, struct tm *result);
+#endif /* !HAVE_LOCALTIME_R */
+
 #if !HAVE_MALLOC
-void *rpl_malloc (size_t n)
+void *rpl_malloc (size_t n);
 #endif /* !HAVE_MALLOC */
 
 #if !HAVE_REALLOC
-void *rpl_realloc (void *p, size_t n)
+void *rpl_realloc (void *p, size_t n);
 #endif /* !HAVE_REALLOC */
+
+#if !HAVE_SETENV
+int setenv(const char *name, const char *value, int overwrite);
+#endif /* !HAVE_SETENV */
 
 #if !HAVE_SLEEP
 unsigned int sleep (unsigned int seconds);
 #endif /* !HAVE_SLEEP */
 
 #if !HAVE_STRDUP
-char *strdup (const char *str)
+char *strdup (const char *str);
 #endif /* !HAVE_STRDUP */
 
 #if !HAVE_STRSIGNAL
 const char *strsignal(int sig);
 #endif /* !HAVE_STRSIGNAL */
+
+#if !HAVE_UNSETENV
+void unsetenv(const char *name);
+#endif /* !HAVE_UNSETENV */
 
 /* silence warnings about an empty library */
 void ck_do_nothing (void);
