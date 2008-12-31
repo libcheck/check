be9b43fc6ade6e8f6314237f00c965069a25aefa
cpickett <cpickett@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2008-12-31 02:54:03 +0000

    * run GNU indent on check_thread_stress.c, and then adjust whitespace
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@500 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- tests/check_thread_stress.c
+++ tests/check_thread_stress.c
@@ -6,8 +6,9 @@
 #include <stdio.h>
 #include <check.h>
 
+/* fork() */
 #ifdef HAVE_UNISTD_H
-#include <unistd.h> /* fork() */
+#include <unistd.h> 
 #endif
 
 #ifdef HAVE_PTHREAD
@@ -18,61 +19,63 @@ Suite *s;
 TCase *tc;
 SRunner *sr;
 
-static void *sendinfo(void *userdata CK_ATTRIBUTE_UNUSED)
+static void *
+sendinfo (void *userdata CK_ATTRIBUTE_UNUSED)
 {
-	unsigned int i;
-	for (i=0; i < 999; i++) {
-  		fail_unless(1,"Shouldn't see this message");
-	}
+  unsigned int i;
+  for (i = 0; i < 999; i++)
+    {
+      fail_unless (1, "Shouldn't see this message");
+    }
 
-	return NULL;
+  return NULL;
 }
 
-
-START_TEST(test_stress_threads)
+START_TEST (test_stress_threads)
 {
 #ifdef HAVE_PTHREAD
-	pthread_t a, b;
-	pthread_create(&a, NULL, sendinfo, (void *) 0xa);
-	pthread_create(&b, NULL, sendinfo, (void *) 0xb);
+  pthread_t a, b;
+  pthread_create (&a, NULL, sendinfo, (void *) 0xa);
+  pthread_create (&b, NULL, sendinfo, (void *) 0xb);
 
-	pthread_join(a, NULL);
-	pthread_join(b, NULL);
+  pthread_join (a, NULL);
+  pthread_join (b, NULL);
 #else
-	sendinfo((void *) 0xa);
-	sendinfo((void *) 0xb);
+  sendinfo ((void *) 0xa);
+  sendinfo ((void *) 0xb);
 #endif
 }
 END_TEST
 
-START_TEST(test_stress_forks)
+START_TEST (test_stress_forks)
 {
-	pid_t cpid = fork();
-	if (cpid == 0) { /* Child */
-		sendinfo((void *) 0x1);
-	} else {
-		sendinfo((void *) 0x2);
-	}
+  pid_t cpid = fork ();
+  if (cpid == 0)
+    {				
+      /* child */
+      sendinfo ((void *) 0x1);
+    }
+  else
+    {
+      sendinfo ((void *) 0x2);
+    }
 }
-END_TEST
+END_TEST 
 
-
-int main(void)
+int
+main (void)
 {
   int nf;
   s = suite_create ("ForkThreadStress");
   tc = tcase_create ("ForkThreadStress");
   sr = srunner_create (s);
-  suite_add_tcase(s, tc);
+  suite_add_tcase (s, tc);
 
   tcase_add_loop_test (tc, test_stress_threads, 0, 100);
   tcase_add_loop_test (tc, test_stress_forks, 0, 100);
 
-  srunner_run_all(sr, CK_SILENT);
-
+  srunner_run_all (sr, CK_SILENT);
   nf = srunner_ntests_failed (sr);
-
-  srunner_free(sr);
-
+  srunner_free (sr);
   return nf ? EXIT_FAILURE : EXIT_SUCCESS;
 }
