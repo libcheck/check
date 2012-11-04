fcc32ce82984beee363b56a6208b10f5c577aeea
brarcher <brarcher@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2012-11-04 03:23:41 +0000

    silence warnings when assigning constant string to char*
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@649 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- src/check_print.c
+++ src/check_print.c
@@ -136,8 +136,8 @@ void tr_fprint (FILE *file, TestResult *tr, enum print_output print_mode)
 void tr_xmlprint (FILE *file, TestResult *tr, enum print_output print_mode CK_ATTRIBUTE_UNUSED)
 {
   char result[10];
-  char *path_name = "";
-  char *file_name = "";
+  char *path_name = (char*)"";
+  char *file_name = (char*)"";
   char *slash = NULL;
 
   switch (tr->rtype) {
