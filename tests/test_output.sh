ecb14ca6c36319737e8d80e2693e4309bb166e69
brarcher <brarcher@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2013-09-22 23:29:05 +0000

    modify test_output.sh to compare output based on HAVE_FORK
    The output of ex_output changes based on HAVE_FORK, as a test is
    skipped when fork is unavailable. However, simply adding another
    string mentioning one less test is not enough to get this working
    for Windows...
    
    When run in Windows, the output differs slightly from when it is run
    under *NIX due to line endings. *NIX uses \n, whereas Windows
    uses \r\n. To account for this, printf is used to generate the
    Windows expected string.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@780 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- tests/test_output.sh
+++ tests/test_output.sh
@@ -1,35 +1,72 @@
 #!/bin/sh
 
+# For this test script, it is assumed that HAVE_FORK=0 implies
+# running in a Windows environment. That means not only are
+# fork-related tests not run, but also line endings are important.
+# In *NIX environments line endings are \n, but in Windows they
+# are expected to be \r\n. For this reason, HAVE_FORK=0 uses
+# printf to generate the comparison strings, so the line endings
+# can be controlled.
+
 . ./test_vars
 
-if [ "${srcdir}" = "." ]; then
-    lsrc=""
-else
-    lsrc="${srcdir}/"
-fi
-
 t0="x"
+
+if [ $HAVE_FORK -eq 1 ]; then
 t1="xRunning suite(s): Master
 33%: Checks: 3, Failures: 1, Errors: 1"
+else
+t1=`printf "xRunning suite(s): Master\r
+50%%: Checks: 2, Failures: 1, Errors: 0\r
+"`
+fi
+
+if [ $HAVE_FORK -eq 1 ]; then
 t2="xRunning suite(s): Master
 33%: Checks: 3, Failures: 1, Errors: 1
-${lsrc}ex_output.c:17:F:Core:test_fail:0: Failure
-${lsrc}ex_output.c:26:E:Core:test_exit:0: (after this point) Early exit with return value 1"
+ex_output.c:17:F:Core:test_fail:0: Failure
+ex_output.c:26:E:Core:test_exit:0: (after this point) Early exit with return value 1"
+else
+t2=`printf "xRunning suite(s): Master\r
+50%%: Checks: 2, Failures: 1, Errors: 0\r
+ex_output.c:17:F:Core:test_fail:0: Failure\r
+"`
+fi
+
+if [ $HAVE_FORK -eq 1 ]; then
 t3="xRunning suite(s): Master
 33%: Checks: 3, Failures: 1, Errors: 1
-${lsrc}ex_output.c:11:P:Core:test_pass:0: Passed
-${lsrc}ex_output.c:17:F:Core:test_fail:0: Failure
-${lsrc}ex_output.c:26:E:Core:test_exit:0: (after this point) Early exit with return value 1"
+ex_output.c:11:P:Core:test_pass:0: Passed
+ex_output.c:17:F:Core:test_fail:0: Failure
+ex_output.c:26:E:Core:test_exit:0: (after this point) Early exit with return value 1"
+else
+t3=`printf "xRunning suite(s): Master\r
+50%%: Checks: 2, Failures: 1, Errors: 0\r
+ex_output.c:11:P:Core:test_pass:0: Passed\r
+ex_output.c:17:F:Core:test_fail:0: Failure\r
+"`
+fi
+
+if [ $HAVE_FORK -eq 1 ]; then
 t4="xtest: Core:test_pass
 success: Core:test_pass
 test: Core:test_fail
 failure: Core:test_fail [
-${lsrc}ex_output.c:17: Failure
+ex_output.c:17: Failure
 ]
 test: Core:test_exit
 error: Core:test_exit [
-${lsrc}ex_output.c:26: (after this point) Early exit with return value 1
+ex_output.c:26: (after this point) Early exit with return value 1
 ]"
+else
+t4=`printf "xtest: Core:test_pass\r
+success: Core:test_pass\r
+test: Core:test_fail\r
+failure: Core:test_fail [\r
+ex_output.c:17: Failure\r
+]\r
+"`
+fi
 
 op0=`./ex_output${EXEEXT} CK_SILENT`
 op1=`./ex_output${EXEEXT} CK_MINIMAL`
