601700e18ab16e7591595f341bf30c2f9002c788
brarcher <brarcher@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2013-09-23 13:06:58 +0000

    modify test_check_nofork.sh to compare output based on HAVE_FORK
    When run in Windows, the output differs slightly from when it is run
    under *NIX due to line endings. *NIX uses \n, whereas Windows
    uses \r\n. To account for this, printf is used to generate the
    Windows expected string.
    
    One other change is not using stderr for the test. Wine has an issue
    with the tmpfile() call, and will output:
       fixme:msvcrt:MSVCRT__sopen_s : pmode 0x0033 ignored
    to stderr when tmpfile() is called. Besides, if the test actually
    does segfault in the future (which is the reason for grabbing the stderr),
    the result will not be to print out the test results.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@781 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- tests/test_check_nofork.sh
+++ tests/test_check_nofork.sh
@@ -1,16 +1,37 @@
 #!/bin/sh
 
+# For this test script, it is assumed that HAVE_FORK=0 implies
+# running in a Windows environment. That means not only are
+# fork-related tests not run, but also line endings are important.
+# In *NIX environments line endings are \n, but in Windows they
+# are expected to be \r\n. For this reason, HAVE_FORK=0 uses
+# printf to generate the comparison strings, so the line endings
+# can be controlled.
+
 . ./test_vars
 
+if [ $HAVE_FORK -eq 1 ]; then
 expected="Running suite(s): NoFork
 0%: Checks: 1, Failures: 1, Errors: 0"
+else
+expected=`printf "Running suite(s): NoFork\r
+0%%: Checks: 1, Failures: 1, Errors: 0\r
+"`
+fi
 
-actual=`./check_nofork${EXEEXT} 2>&1`
+actual=`./check_nofork${EXEEXT}`
 if [ x"${expected}" = x"${actual}" ]; then
   exit 0
 else
   echo "Problem with check_nofork${EXEEXT}"
-  echo "Expected: Failure"
-  echo "Got: Segmentation fault"
+  echo "Expected: "
+  echo "${expected}"
+  echo "Got: "
+  echo "${actual}"
+  
+  echo xxd expected
+  echo -n "${expected}" | xxd
+  echo xxd actual
+  echo -n "${actual}" | xxd
   exit 1
 fi
