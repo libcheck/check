2f580655885e8aae7823baf11658d80797a6c141
brarcher <brarcher@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2013-09-23 13:07:01 +0000

    modify test_log_output.sh to compare output based on HAVE_FORK
    When run in Windows, the output differs slightly from when it is run
    under *NIX due to line endings. *NIX uses \n, whereas Windows
    uses \r\n. To account for this, printf is used to generate the
    Windows expected string.
    
    Additionally, the test_exit test was removed, as it is not valid
    if fork is unavailable.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@782 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- tests/test_log_output.sh
+++ tests/test_log_output.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
 
+# For this test script, it is assumed that HAVE_FORK=0 implies
+# running in a Windows environment. That means not only are
+# fork-related tests not run, but also line endings are important.
+# In *NIX environments line endings are \n, but in Windows they
+# are expected to be \r\n. For this reason, HAVE_FORK=0 uses
+# printf to generate the comparison strings, so the line endings
+# can be controlled.
+
 . ./test_vars
 
-if [ "${srcdir}" = "." ]; then
-    lsrc=""
-else
-    lsrc="${srcdir}/"
-fi
-
+if [ $HAVE_FORK -eq 1 ]; then
 expected="Running suite S1
-${lsrc}ex_log_output.c:10:P:Core:test_pass:0: Passed
-${lsrc}ex_log_output.c:16:F:Core:test_fail:0: Failure
-${lsrc}ex_log_output.c:20:E:Core:test_exit:0: (after this point) Early exit with return value 1
+ex_log_output.c:10:P:Core:test_pass:0: Passed
+ex_log_output.c:16:F:Core:test_fail:0: Failure
+ex_log_output.c:25:E:Core:test_exit:0: (after this point) Early exit with return value 1
 Running suite S2
-${lsrc}ex_log_output.c:28:P:Core:test_pass2:0: Passed
+ex_log_output.c:34:P:Core:test_pass2:0: Passed
 Results for all suites run:
 50%: Checks: 4, Failures: 1, Errors: 1"
-
+else
+expected=`printf "Running suite S1\r
+ex_log_output.c:10:P:Core:test_pass:0: Passed\r
+ex_log_output.c:16:F:Core:test_fail:0: Failure\r
+Running suite S2\r
+ex_log_output.c:34:P:Core:test_pass2:0: Passed\r
+Results for all suites run:\r
+66%%: Checks: 3, Failures: 1, Errors: 0\r
+"`
+fi
 
 test_log_output ( ) {
     
