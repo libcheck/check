2bc1c4f2a0f4fa383f3603b37eee4d3a3f2f9506
brarcher <brarcher@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2013-09-28 00:33:53 +0000

    check_check: do not run tests using setenv if unavailable
    If the system does not have setenv(), do not use it during
    tests. The libcompat version calls assert(), failing the test.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@805 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- tests/check_check_selective.c
+++ tests/check_check_selective.c
@@ -198,6 +198,7 @@ START_TEST(test_srunner_suite_no_tcase)
 END_TEST
 
 
+#if HAVE_WORKING_SETENV
 START_TEST(test_srunner_run_suite_env)
 {
   /* This test makes the srunner_run_all function to run all the test
@@ -303,6 +304,7 @@ START_TEST(test_srunner_suite_no_tcase_env)
   unsetenv ("CK_RUN_CASE");
 }
 END_TEST
+#endif /* HAVE_WORKING_SETENV */
 
 Suite *make_selective_suite (void)
 {
@@ -317,12 +319,15 @@ Suite *make_selective_suite (void)
   tcase_add_test (tc, test_srunner_no_tcase);
   tcase_add_test (tc, test_srunner_suite_tcase);
   tcase_add_test (tc, test_srunner_suite_no_tcase);
+
+#if HAVE_WORKING_SETENV
   tcase_add_test (tc, test_srunner_run_suite_env);
   tcase_add_test (tc, test_srunner_run_no_suite_env);
   tcase_add_test (tc, test_srunner_run_tcase_env);
   tcase_add_test (tc, test_srunner_no_tcase_env);
   tcase_add_test (tc, test_srunner_suite_tcase_env);
   tcase_add_test (tc, test_srunner_suite_no_tcase_env);
+#endif /* HAVE_WORKING_SETENV */
 
   tcase_add_unchecked_fixture (tc,
                                selective_setup,
