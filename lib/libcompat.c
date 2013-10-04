0b978f4bf78c9bb708b53a3f3dfc4d2729a44458
brarcher <brarcher@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2013-10-04 01:38:08 +0000

    silence warning about ck_do_nothing being able to return
    The prototype for ck_do_nothing marks it as noreturn.
    gcc thought that the function could still return. Adding
    an exit() call at the end (although the assert() will not
    let it get that far) to silence the warning.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@828 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- lib/libcompat.c
+++ lib/libcompat.c
@@ -5,4 +5,11 @@ void
 ck_do_nothing (void)
 {
   assert (0);
+
+  /*
+   * to silence warning about this function actually
+   * returning, but being marked as noreturn. assert()
+   * must be marked as a function that returns.
+   */
+  exit(1);
 }
