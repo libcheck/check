9c152fe43b547279e85d141fe94006e56c4d0111
brarcher <brarcher@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2013-10-04 01:38:17 +0000

    silence warning about wrong type passed to localtime_r
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@831 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- src/check_log.c
+++ src/check_log.c
@@ -240,7 +240,7 @@ void xml_lfun (SRunner *sr CK_ATTRIBUTE_UNUSED, FILE *file, enum print_output pr
     struct tm now;
     gettimeofday(&inittv, NULL);
     clock_gettime(check_get_clockid(), &ts_start);
-    localtime_r(&(inittv.tv_sec), &now);
+    localtime_r((const time_t*)&(inittv.tv_sec), &now);
     strftime(t, sizeof("yyyy-mm-dd hh:mm:ss"), "%Y-%m-%d %H:%M:%S", &now);
   }
 
