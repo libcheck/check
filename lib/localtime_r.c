377ad6a5196aa2f3a5e62a661a589458dee3a4ed
brarcher <brarcher@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2013-12-25 03:04:02 +0000

    localtime_r.c: provide additional alternatives
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@930 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- lib/localtime_r.c
+++ lib/localtime_r.c
@@ -1,19 +1,24 @@
 #include "libcompat.h"
 
 #if !defined(localtime_r)
-struct tm *
-localtime_r (const time_t *clock, struct tm *result)
+
+struct tm * localtime_r (const time_t *clock, struct tm *result)
 {
-  struct tm *now = localtime (clock);
-  if (now == NULL)
-    {
-      return NULL;
-    }
-  else
+    struct tm *now = NULL;
+
+#if HAVE_LOCALTIME_S
+    localtime_s (now, clock)
+#elif HAVE__LOCALTIME64_S
+    _localtime64_s (now, clock);
+#else
+    now = localtime (clock);
+#endif /* !HAVE_LOCALTIME_S */
+
+    if (now != NULL)
     {
       *result = *now;
     }
-  return result;
+    return result;
 }
 
-#endif
+#endif /* !defined(localtime_r) */
