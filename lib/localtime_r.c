8b38707994785a02c4f0f9bfa1f5af5e089f4d41
brarcher <brarcher@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2014-01-06 04:41:41 +0000

    libcompat: remove alternatives for localtime_r
    returning to the previous localtime_r implementation that uses
    localtime. The _localtime64_s implementation had issues, in that
    it wanted to write to the "now" variable. (Should have been
    passed "result" instead). Further, on MSVC (for which this replacement
    is being considered) localtime redirects to _localtime64_s anyway.
    
    For how Check is using localtime_r, just using localtime is
    sufficient.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/check/code/trunk@1038 64e312b2-a51f-0410-8e61-82d0ca0eb02a

--- lib/localtime_r.c
+++ lib/localtime_r.c
@@ -4,20 +4,16 @@
 
 struct tm * localtime_r (const time_t *clock, struct tm *result)
 {
-    struct tm *now = NULL;
-
-#if HAVE_LOCALTIME_S
-    localtime_s (now, clock)
-#elif HAVE__LOCALTIME64_S
-    _localtime64_s (now, clock);
-#else
-    now = localtime (clock);
-#endif /* !HAVE_LOCALTIME_S */
-
-    if (now != NULL)
+    struct tm *now = localtime (clock);
+    if (now == NULL)
     {
-      *result = *now;
+        return NULL;
     }
+    else
+    {
+        *result = *now;
+    }
+
     return result;
 }
 
