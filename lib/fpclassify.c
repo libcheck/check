a1e98d6b1e79cd5064240b57411203d704e5feea
bel2125 <bel2125@gmail.com>
2017-09-01 10:47:03 +0200

    Set license statement

--- lib/fpclassify.c
+++ lib/fpclassify.c
@@ -1,9 +1,24 @@
-/* Replacement function for isnan/isinf/isfinite for processors using
- * IEEE 754 floating point format.
- * Copyright (C) 2017, bel2125
- * License: LGPL v2 or MIT (as you like).
+/* Copyright (C) 2017, bel2125
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
  */
-
+/* Note: Within "check", this file uses the LGPL license.
+ * If you need only this file, you may find a MIT licensed version in
+ * the "floatmagic" repository of the initial author.
+ */
 
 #include "libcompat.h"
 
