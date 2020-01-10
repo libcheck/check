b7af21c04df406945b41d20eb9c99255b5951b3e
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2020-01-10 21:03:06 +0100

    Change C++ comment to C style, clean another comment

--- lib/gettimeofday.c
+++ lib/gettimeofday.c
@@ -32,7 +32,7 @@ int gettimeofday(struct timeval *tv, void *tz)
 #if defined(_MSC_VER)
     union
     {
-        __int64 ns100;          /*time since 1 Jan 1601 in 100ns units */
+        __int64 ns100;          /* time since 1 Jan 1601 in 100ns units */
         FILETIME ft;
     } now;
 
@@ -41,7 +41,7 @@ int gettimeofday(struct timeval *tv, void *tz)
     tv->tv_sec = (long)((now.ns100 - EPOCHFILETIME) / 10000000LL);
     return (0);
 #else
-    // Return that there is no implementation of this on the system
+    /* Return that there is no implementation of this on the system */
     errno = ENOSYS;
     return -1;
 #endif /* _MSC_VER */
